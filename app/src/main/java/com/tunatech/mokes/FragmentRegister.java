d397bf0590e1f57337d6938b73f8c1efcca05209
Sandi Andrian <andrian.sandi@gmail.com>
2016-02-27 21:10:53 +0700

    Initial commit

---
+++ app/src/main/java/com/tunatech/mokes/FragmentRegister.java
@@ -0,0 +1,193 @@
+package com.tunatech.mokes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.w3c.dom.Text;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+/**
+ * Created by royyan on 2/9/2016.
+ */
+
+public class FragmentRegister extends Fragment {
+
+    private EditText etFName, etUserName, etPhoneNo, etEmail, etPass;
+    private IFragRegister context;
+
+    public interface IFragRegister {
+        void FinishRegister(int result, Intent data);
+    }
+
+    public static FragmentRegister newInstance() {
+        FragmentRegister f = new FragmentRegister();
+        return f;
+    }
+
+    public FragmentRegister() {
+        Log.d(LoginActivity.TAG, "construct fragment register");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(LoginActivity.TAG, "onCreateView fragment register");
+        View view = inflater.inflate(R.layout.login_frag_form_register, container, false);
+
+        etFName = (EditText) view.findViewById(R.id.form_reg_full_name);
+        etUserName = (EditText) view.findViewById(R.id.form_reg_user_name);
+        etPhoneNo = (EditText) view.findViewById(R.id.form_reg_phone_number);
+        etEmail = (EditText) view.findViewById(R.id.form_reg_email);
+        etPass = (EditText) view.findViewById(R.id.form_reg_password);
+
+
+        Button btnRegister = (Button) view.findViewById(R.id.form_register_button);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegister();
+            }
+        });
+
+        View backToLogin = (View) view.findViewById(R.id.form_back_to_login);
+        backToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.FinishRegister(LoginCantActivity.RESULT_CANCELED, null);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = (IFragRegister) context;
+    }
+
+    private void attemptRegister() {
+
+        EditText[] etarry = {etFName, etUserName, etPhoneNo, etEmail, etPass};
+        // Reset errors.
+        for (EditText item : etarry) {
+            item.setError(null);
+        }
+
+
+
+        boolean cancel = false;
+        View focusView = null;
+        for (EditText item : etarry) {
+            String input_data = item.getText().toString();
+            if (TextUtils.isEmpty(input_data)) {
+                cancel = true;
+                item.setError(getString(R.string.error_field_required));
+                focusView = item;
+                break;
+            }
+
+            if (item == etPass && !isPasswordValid(input_data)) {
+                item.setError(getString(R.string.error_invalid_password));
+                focusView = item;
+                cancel = true;
+                break;
+            }
+
+            if (item == etEmail && !isEmailValid(input_data)) {
+                item.setError(getString(R.string.error_invalid_email));
+                focusView = item;
+                cancel = true;
+                break;
+            }
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                    .create();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://mokes.kodrindonesia.com/api/")
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+
+            UserApi user_api = retrofit.create(UserApi.class);
+
+            Call<Users> call = user_api.Register("rezaandroid", "reza@gmailandroid.com");
+
+            call.enqueue(new Callback<Users>() {
+                @Override
+                public void onResponse(Response<Users> response, Retrofit retrofit) {
+//                   if(response.body().code=="200") {
+                       Toast.makeText(getActivity(),("Registrasi Berhasil"),Toast.LENGTH_SHORT).show();
+//                   }
+//                    for (Users.UserItem user : response.body().getUsers()) {
+//                        etPass.append("Id = " + String.valueOf(user.getId()) +
+//                                        System.getProperty("line.separator") +
+//                                        "Email = " + user.getEmail() +
+//                                        System.getProperty("line.separator") +
+//                                        "Password = " + user.getPassword() +
+//                                        System.getProperty("line.separator") +
+//                                        "Token Auth = " + user.getToken_auth() +
+//                                        System.getProperty("line.separator") +
+//                                        "Name = " + user.getName() +
+//                                        System.getProperty("line.separator") +
+//                                        "Updated at = " + user.getUpdated_at() +
+//                                        System.getProperty("line.separator")
+//                        );
+//                    }
+                }
+
+                @Override
+                public void onFailure(Throwable t)
+                {
+                    etEmail.setText(String.valueOf(t));}
+
+            });
+
+
+        }
+
+
+//        if (cancel) {
+//            focusView.requestFocus();
+//        } else {
+//
+//            Intent goingBack = new Intent();
+//            for(EditText item : etarry) {
+//                goingBack.putExtra(String.valueOf(item.getId()),item.getText().toString());
+//            }
+//            context.FinishRegister(LoginCantActivity.RESULT_OK, goingBack);
+//
+//        }
+    }
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+}
---
+++ app/src/main/java/com/tunatech/mokes/User.java
@@ -0,0 +1,70 @@
+package com.tunatech.mokes;
+
+/**
+ * Created by User on 17/02/2016.
+ */
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String token_auth;
+    private String name;
+    private String updated_at;
+    private String code;
+
+
+    public int getId() {
+        return id;
+
+    }
+    public void setId() {
+        this.id = id;
+
+    }
+    public String getEmail() {
+        return email;
+
+    }
+    public void setEmail(String email) {
+        this.email = email;
+
+    }
+    public String getPassword() {
+        return password;
+
+    }
+    public void setPassword(String password) {
+        this.password = password;
+
+    }
+    public String getToken_auth() {
+        return token_auth;
+
+    }
+    public void setToken_auth(String token_auth) {
+        this.token_auth = token_auth;
+
+    }
+    public String getName() {
+        return name;
+
+    }
+    public void setName(String name) {
+        this.name = name;
+
+    }
+    public String getUpdated_at() {
+        return updated_at;
+
+    }
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
