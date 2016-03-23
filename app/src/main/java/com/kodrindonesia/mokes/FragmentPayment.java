0f3a61e9408ba0268f1d94146b6616c0aa7453ad
Reza Ramdhanisti <Rezaramdhanisti@gmail.com>
2016-03-23 16:51:33 +0700

    revisi 23 maret

--- app/src/main/java/com/kodrindonesia/mokes/FragmentPayment.java
+++ app/src/main/java/com/kodrindonesia/mokes/FragmentPayment.java
@@ -8,8 +8,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 /**
  * Created by royyan on 2/11/2016.
@@ -36,20 +34,20 @@ public class FragmentPayment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_payment,container,false);
 
-        Button btnTopUp = (Button) view.findViewById(R.id.paymmnt_btn_topup);
-        Button btnNotNow = (Button) view.findViewById(R.id.paymmnt_btn_not_now);
-
-        Button[] btn_ctrl = {btnTopUp, btnNotNow};
-
-        for (Button element: btn_ctrl) {
-            element.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onBtnCliked(v);
-                }
-            });
-        }
-
+//        Button btnTopUp = (Button) view.findViewById(R.id.paymmnt_btn_topup);
+//        Button btnNotNow = (Button) view.findViewById(R.id.paymmnt_btn_not_now);
+//
+//        Button[] btn_ctrl = {btnTopUp, btnNotNow};
+//
+//        for (Button element: btn_ctrl) {
+//            element.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    onBtnCliked(v);
+//                }
+//            });
+//        }
+//
         return view;
     }
 
@@ -59,18 +57,18 @@ public class FragmentPayment extends Fragment {
         mainActivity = (MainActivity) context;
     }
 
-    private void onBtnCliked(View item){
-        int id = item.getId();
-
-        if (id == R.id.paymmnt_btn_topup) {
-            Toast.makeText(getContext(), "top up clicked", Toast.LENGTH_SHORT).show();
-            mainActivity.setupFragmentUser();
-            mainActivity.setupFragmentTopUp();
-        }
-        else if (id == R.id.paymmnt_btn_not_now) {
-            Toast.makeText(getContext(), "not now clicked", Toast.LENGTH_SHORT).show();
-            mainActivity.onBackPressed();
-        }
-    }
+//    private void onBtnCliked(View item){
+//        int id = item.getId();
+//
+//        if (id == R.id.paymmnt_btn_topup) {
+//            Toast.makeText(getContext(), "top up clicked", Toast.LENGTH_SHORT).show();
+//            mainActivity.setupFragmentUser();
+//            mainActivity.setupFragmentTopUp();
+//        }
+//        else if (id == R.id.paymmnt_btn_not_now) {
+//            Toast.makeText(getContext(), "not now clicked", Toast.LENGTH_SHORT).show();
+//            mainActivity.onBackPressed();
+//        }
+//    }
     }
 
